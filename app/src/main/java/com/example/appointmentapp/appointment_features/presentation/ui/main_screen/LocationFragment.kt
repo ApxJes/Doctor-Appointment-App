c6285ac9560faac2ce509390ed5d414652c74988
AgPyae Kyaw <Shwyzee109@gmail.com>
2025-06-10 22:10:11 +0630

    feat: Get General Hospital Yangon Location and appear on google map

--- app/src/main/java/com/example/appointmentapp/appointment_features/presentation/ui/main_screen/LocationFragment.kt
+++ app/src/main/java/com/example/appointmentapp/appointment_features/presentation/ui/main_screen/LocationFragment.kt
@@ -1,11 +1,13 @@
 package com.example.appointmentapp.appointment_features.presentation.ui.main_screen
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.createBitmap
 import androidx.fragment.app.Fragment
 import com.example.appointmentapp.R
 import com.example.appointmentapp.databinding.FragmentLocationBinding
@@ -13,7 +15,10 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -21,6 +26,7 @@ class LocationFragment : Fragment(), OnMapReadyCallback {
     private var _binding: FragmentLocationBinding? = null
     private val binding get() = _binding!!
     private var map: GoogleMap? = null
+    private var activeMarker: Marker? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -61,8 +67,50 @@ class LocationFragment : Fragment(), OnMapReadyCallback {
         map?.mapType = GoogleMap.MAP_TYPE_NORMAL
 
         val yangonLatLong = LatLng(16.8409, 96.1735)
+
+        val generalHospitalLatLong = LatLng(16.7791, 96.1490)
+        val generalHospitalMarker = MarkerOptions()
+        generalHospitalMarker.position(generalHospitalLatLong)
+        generalHospitalMarker.title("Location")
+        generalHospitalMarker.snippet("General Hospital Yangon")
+        generalHospitalMarker.icon(BitmapDescriptorFactory.fromBitmap(getBitMapFromDrawable(R.drawable.hospital_icon)))
+        val marker = map?.addMarker(generalHospitalMarker)
+        marker?.tag = WindowInfoData(
+            title = "General Hospital Yangon",
+            des = getString(R.string.general_hospital_yangon),
+            image = R.drawable.general_hospital_yangon
+        )
+
+        map?.setInfoWindowAdapter(CustomWindowInfoAdapter(requireContext()))
+        map?.setOnMarkerClickListener { marker ->
+            activeMarker = marker
+            marker.showInfoWindow()
+            true
+        }
+
+        map?.setOnMapClickListener {
+            activeMarker?.hideInfoWindow()
+        }
+
+        map?.setOnCameraMoveListener {
+            activeMarker?.hideInfoWindow()
+        }
+
         map?.uiSettings?.isZoomControlsEnabled = true
         map?.animateCamera(CameraUpdateFactory.newLatLngZoom(yangonLatLong, 12f))
+    }
+
+    private fun getBitMapFromDrawable(resId: Int): Bitmap? {
+        var bitmap: Bitmap? = null
+        val drawable = ResourcesCompat.getDrawable(resources, resId, null)
+        drawable?.let {
+            bitmap = createBitmap(120, 120)
+            val canvas = Canvas(bitmap)
+            it.setBounds(0, 0, canvas.width, canvas.height)
+            it.draw(canvas)
+        }
+
+        return bitmap
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/example/appointmentapp/appointment_features/presentation/ui/main_screen/WindowInfoData.kt
@@ -0,0 +1,7 @@
+package com.example.appointmentapp.appointment_features.presentation.ui.main_screen
+
+data class WindowInfoData(
+    val title: String,
+    val des: String,
+    val image: Int
+)
